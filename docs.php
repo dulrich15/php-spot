aa762204a7f7376d7dbf74afd552da55d7e85518
Dave (win-xp) <none@example.com>
2013-01-02 16:06:28 -0800

    Reverted css to old look -- internal is still new

--- docs.php
+++ docs.php
@@ -1,9 +1,12 @@
 <?php 
 
-$doc_folder = "2013p201";
+if ( $qtr == 201 ) $doc_folder = "2013p201";
+if ( $qtr == 202 ) $doc_folder = "2013p202";
+if ( $qtr == 203 ) $doc_folder = "2013p203";
+
 $meta_folder = "meta";
+$supp_folder = "supp";
 $nbr_weeks = 10;
-$max_access = 2;
 
 function read_csv($tablename)
 {
@@ -39,82 +42,108 @@ function read_csv($tablename)
     return $csv_array;
 }
 
-
-/* Load data */
-
-if ( $_SESSION['id'] ) 
+if ( ! is_dir($doc_folder) )
 {
-    $users = read_csv('users');
-    $user = '';
-    foreach ( $users as $key => $value )
-    {
-        if ( $_SESSION['id'] == md5($value['id']) )
-        {
-            $user = $value;
-        }
-    }
+    $docs = "WIP";
 }
-$max_access = $user['access'];
-
-$doctypes_raw = read_csv('doctypes');
-foreach ( $doctypes_raw as $doctype )
+else
 {
-    foreach ( $doctype as $key => $value )
+    if ( $_SESSION['id'] ) 
     {
-        if ( $key != 'id' )
+        $users = read_csv('users');
+        $user = '';
+        foreach ( $users as $key => $value )
         {
-            $doctypes[$doctype['id']][$key] = $value;
-        }
-    }
-}
-$docs = read_csv('docs');
-foreach ( $docs as $doc )
-{
-    $doctypes[$doc['type_id']]['docs'][] = $doc;
-}
-
-
-/* Build doc table */
-
-
-$body = "<table>\n";
-for ( $a = 0; $a <= $max_access; $a++ )
-{
-    $body .= "<p>\n";
-    foreach ( $doctypes as $doctype ) 
-    {
-        if ( $doctype['access'] + 1 == $a + 1 )
-        {
-            $body .= "<tr>\n";
-            if ( $doctype['weekly'] )
+            if ( $_SESSION['id'] == md5($value['id']) )
             {
-                $body .= "<td>" . $doctype['type'] . "</td>\n";
-                $n = 1;
-                for ( $w = 1; $w <= $nbr_weeks; $w++ )
+                $user = $value;
+            }
+        }
+    }
+    $max_access = $user['access'];
+
+    $doctypes_raw = read_csv('doctypes');
+    foreach ( $doctypes_raw as $doctype )
+    {
+        foreach ( $doctype as $key => $value )
+        {
+            if ( $key != 'id' )
+            {
+                $doctypes[$doctype['id']][$key] = $value;
+            }
+        }
+    }
+    $docs = read_csv('docs');
+    foreach ( $docs as $doc )
+    {
+        $doctypes[$doc['type_id']]['docs'][] = $doc;
+    }
+
+
+    /* Build doc table */
+
+
+    $docs = "<p>\n";
+    $docs .= "<table>\n";
+    $docs .= "<tr>\n";
+    $docs .= "<th class=\"c\">Week:</th>\n";
+    for ( $w = 1; $w <= $nbr_weeks; $w++ )
+    {
+        $docs .= "<th class=\"c\">" . $w . "</th>\n";
+    }
+    $docs .= "</tr>\n";
+    for ( $a = 0; $a <= $max_access; $a++ )
+    {
+        foreach ( $doctypes as $doctype ) 
+        {
+            if ( $doctype['access'] + 1 == $a + 1 )
+            {
+                $docs .= "<tr>\n";
+                if ( $doctype['weekly'] )
                 {
-                    $body .= "<td class=\"c\">";
-                    foreach ( $doctype['docs'] as $doc )
+                    $docs .= "<td class=\"l\">" . $doctype['type'] . "</td>\n";
+                    $n = 1;
+                    for ( $w = 1; $w <= $nbr_weeks; $w++ )
                     {
-                        if ( $doc['week'] == $w )
+                        $docs .= "<td class=\"c\">";
+                        foreach ( $doctype['docs'] as $doc )
                         {
-                            $body .= "<a href=\"" . $doc_folder . "/" . $doc['filename'] . "\">" . $n . "</a>\n";
-                            $n++;
+                            if ( $doc['week'] == $w )
+                            {
+                                $docs .= "<a href=\"" . $doc_folder . "/" . $doc['filename'] . "\">" . $n . "</a>\n";
+                                $n++;
+                            }
                         }
+                        $docs .= "</td>\n";
                     }
-                    $body .= "</td>\n";
                 }
+                else
+                {
+                    $docs .= "<td class=\"l\">\n";
+                    $docs .= "<a href=\"" . $doc_folder . "/" . $doctype['docs'][0]['filename'] . "\">";
+                    $docs .= $doctype['type'];
+                    $docs .= "</a>";
+                    $docs .= "</td>\n";
+                }
+                $docs .= "</tr>\n";
             }
-            else
-            {
-                $body .= "<td>\n";
-                $body .= "<a href=\"" . $doc_folder . "/" . $doctype['docs'][0]['filename'] . "\">";
-                $body .= $doctype['type'];
-                $body .= "</a>";
-                $body .= "</td>\n";
-            }
-            $body .= "</tr>\n";
         }
     }
+    $docs .= "</table>\n";
+    $docs .= "</p>\n";
+
+    $supp = "";
+    if ( ! is_dir($supp_folder) )
+    {
+        $supp_array = read_csv('supp');
+        $supp .= "<h3>Supplemental Docs</h3>";
+        $supp .= "<ul>";
+        foreach ( $supp_array as $s )
+        {
+            $supp .= "<li><a href=\"" . $doc_folder . "/" . $supp_folder . "/" . $s['filename'] . "\">" . $s['label'] . "</a></li>";
+        }
+        $supp .= "</ul>";
+    }
 }
-$body .= "</table>\n";
+
 ?> 
--- index.php
+++ index.php
@@ -1,26 +1,83 @@
 <?php
+
 session_start();
 $_SESSION['id'] = md5($_POST['id']);
+
+$y = 2013;
+$qtr = ( $_GET['qtr'] ) ? $_GET['qtr'] : 201;
+
+if ( $qtr == 201 ) $q = "Winter $y";
+if ( $qtr == 202 ) $q = "Spring $y";
+if ( $qtr == 203 ) $q = "Summer $y";
+
+include "docs.php";
+
 ?>
 <html>
 <head>
-<style>
-td { 
-    border:1px solid #eee; 
-    padding:5px 10px; 
-}
-.c {
-    text-align:center;
-}
-</style>
+<title>Physics 200 Series</title>
+<link rel='stylesheet' type='text/css' href='main.css' />
 </head>
 <body onLoad="document.forms[0].elements[0].focus()">
+
+<div id='qlnk'><i class='content_descriptor'>Login</i>
 <form method="post" action="">
 <input type="password" name="id" onLoad="this.focus()">
 </form>
-<?php 
-include "docs.php";
-echo $body;
-?>
+</div>
+
+<div id='head'>
+<h1>Physics <?php echo $qtr; ?> Documentation</h1>
+</div>
+
+<div id='menu'><i class='content_descriptor'>Navigation Menu</i>
+<ul class='horizontal'>
+<li><a href='?qtr=201'>201</a></li>
+<li><a href='?qtr=202'>202</a></li>
+<li><a href='?qtr=203'>203</a></li>
+</ul>
+</div>
+
+<div id='main'><i class='content_descriptor'>Main Content</i>
+<h2><?php echo $q; ?></h2>
+<?php echo $docs; ?>
+<?php echo $supp; ?>
+</div>
+
+<div id='supp'><i class='content_descriptor'>Supplemental Content</i>
+<p>Looking for help? Check out <a href="http://physicsforums.com">PhysicsForums.com</a>!</p>
+<h3>General docs</h3>
+<ul>
+<li><a href='resources/si-units.pdf'>SI units</a></li>
+<li><a href='resources/constants.pdf'>Constants</a></li>
+<li><a href='resources/notations.pdf'>Notations</a></li>
+</ul>
+<h3>Archive...</h3>
+<ul>
+<li>2012 Lecture Notes<br>
+<a href='resources/2012p201ln.pdf'>201</a> |
+<a href='resources/2012p202ln.pdf'>202</a> |
+<a href='resources/2012p203ln.pdf'>203</a>
+</li>
+<li>2011 Lecture Notes<br>
+<a href='resources/2011p200ln.pdf'>201&ndash;202&ndash;203</a>
+</li>
+</ul>
+</div>
+
+<div id='info'><i class='content_descriptor'>Additional Info</i>
+<dl>
+<dt>Instructor</dt><dd>David J. Ulrich</dd>
+<dt>E-mail</dt><dd><a href='mailto:david.ulrich15@pcc.edu'>david.ulrich15@pcc.edu</a></dd>
+<dt>Time</dt><dd>Monday, Wednesday <nobr>6:00-8:50 pm</nobr></dd>
+<dt>Location</dt><dd>PCC, Rock Creek Campus</dd><dd>Building 7</dd>
+<dt>Room</dt><dd>223 on Monday</dd><dd>225 on Wednesday</dd>
+</dl>
+</div>
+
+<div id='foot'><i class='content_descriptor'>In Conclusion...</i>
+<p>Site hosting by Portland Community College, <a href='http://www.pcc.edu'>http://www.pcc.edu</a></p>
+</div>
+
 </body>
 </html>
