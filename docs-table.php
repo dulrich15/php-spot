d4af44f9a7f96a00e6fe0331e195147b441ab07c
Dave (win-xp) <none@example.com>
2013-01-03 11:25:20 -0800

    Columnated the table and links

--- docs-table.php
+++ docs-table.php
@@ -42,7 +42,11 @@ function read_csv($tablename)
     return $csv_array;
 }
 
-if ( is_dir($doc_folder) )
+if ( ! is_dir($doc_folder) )
+{
+    $docs = "<p>Work in progress</p>";
+}
+else
 {
     if ( $_SESSION['id'] ) 
     {
@@ -69,68 +73,65 @@ if ( is_dir($doc_folder) )
             }
         }
     }
-
     $docs = read_csv('docs');
-    foreach ( $doctypes_raw as $doctype )
+    foreach ( $docs as $doc )
     {
-        foreach ( $docs as $doc )
-        {
-            if ( $doc['type_id'] == $doctype['id'] )
-            {
-                $doctypes[$doc['type_id']]['docs'][] = $doc;
-            }
-        }
+        $doctypes[$doc['type_id']]['docs'][] = $doc;
     }
 
-    
+
     /* Build doc table */
 
 
-    $docs = "<ul>\n";
-    foreach ( $doctypes as $doctype ) 
-    {
-        if ( ! $doctype['weekly'] )
-        {
-            if ( $doctype['access'] + 1 <= $max_access + 1 )
-            {
-                $docs .= "<li>";
-                $docs .= "<a href=\"" . $doc_folder . "/" . $doctype['docs'][0]['filename'] . "\">";
-                $docs .= $doctype['type'];
-                $docs .= "</a>";
-                $docs .= "</li>\n";
-            }
-        }
-    }
-    $docs .= "</ul>\n";
-    
-    $wkly = "<dl>\n";
+    $docs = "<p>\n";
+    $docs .= "<table>\n";
+    $docs .= "<tr>\n";
+    $docs .= "<th class=\"c\">Week:</th>\n";
     for ( $w = 1; $w <= $nbr_weeks; $w++ )
     {
-        $wkly .= "<dt>Week " . $w . "</dt>\n";
+        $docs .= "<th class=\"c\">" . $w . "</th>\n";
+    }
+    $docs .= "</tr>\n";
+    for ( $a = 0; $a <= $max_access; $a++ )
+    {
         foreach ( $doctypes as $doctype ) 
         {
-            if ( $doctype['weekly'] )
+            if ( $doctype['access'] + 1 == $a + 1 )
             {
-                if ( $doctype['access'] + 1 <= $max_access + 1 )
+                $docs .= "<tr>\n";
+                if ( $doctype['weekly'] )
                 {
-                    foreach ( $doctype['docs'] as $n => $doc )
+                    $docs .= "<td class=\"l\">" . $doctype['type'] . "</td>\n";
+                    $n = 1;
+                    for ( $w = 1; $w <= $nbr_weeks; $w++ )
                     {
-                        if ( $doc['week'] == $w )
+                        $docs .= "<td class=\"c\">";
+                        foreach ( $doctype['docs'] as $doc )
                         {
-                            $weekday = 'MTWHFSU';
-                            $weekday = array('Mon','Tue','Wed','Thu','Fri','Sat','Sun');
-                            $wkly .= "<dd>" . $weekday[$doc['day']] . ": ";
-                            $atext = implode(" " . $doc['n'] . " ", explode(" ", $doctype['type'], 2));
-                            $wkly .= "<a href=\"" . $doc_folder . "/" . $doc['filename'] . "\">" . $atext . "</a>";
-                            $wkly .= "</dd>\n";
+                            if ( $doc['week'] == $w )
+                            {
+                                $docs .= "<a href=\"" . $doc_folder . "/" . $doc['filename'] . "\">" . $n . "</a>\n";
+                                $n++;
+                            }
                         }
+                        $docs .= "</td>\n";
                     }
                 }
+                else
+                {
+                    $docs .= "<td class=\"l\">\n";
+                    $docs .= "<a href=\"" . $doc_folder . "/" . $doctype['docs'][0]['filename'] . "\">";
+                    $docs .= $doctype['type'];
+                    $docs .= "</a>";
+                    $docs .= "</td>\n";
+                }
+                $docs .= "</tr>\n";
             }
         }
     }
-    $wkly .= "</dl>\n";
-                            
+    $docs .= "</table>\n";
+    $docs .= "</p>\n";
+
     $supp = "";
     if ( ! is_dir($supp_folder) )
     {
--- index.php
+++ index.php
@@ -16,73 +16,96 @@ include "docs.php";
 <html>
 
 <head>
+
     <title>Physics 200 Series</title>
     <link rel='stylesheet' type='text/css' href='main.css'>
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+    
 </head>
 
 <body onLoad="document.forms[0].elements[0].focus()">
 
-<h1>Physics <?php echo $qtr; ?> Docs</h1>
+    <header>
 
-<p>
-    <form method="post" action="">
-        Login? <input type="password" name="id" onLoad="this.focus()">
-    </form>
-</p>
+        <h1>Physics <?php echo $qtr; ?>, <?php echo $season; ?></h1>
+        <p>
+            <form method="post" action="">
+                Login? <input type="password" name="id" onLoad="this.focus()">
+            </form>
+        </p>
 
-<h2><?php echo $season; ?></h2>
-<?php echo $docs; ?>
+    </header>
 
-<?php if ( $supp ) { ?>
-<h2>Supplemental docs</h2>
-<?php echo $supp; } ?>
+    <div id="col1">
 
-<h2>Some general docs</h2>
-<ul>
-    <li><a href='resources/si-units.pdf'>SI units</a></li>
-    <li><a href='resources/constants.pdf'>Constants</a></li>
-    <li><a href='resources/notations.pdf'>Notations</a></li>
-</ul>
+        <h2>Weekly docs</h2>
+        <?php echo $wkly; ?>
+        
+    </div>
 
-<h2>My archives...</h2>
-<ul>
-    <li>2012 Lecture Notes<br>
-        <a href='resources/2012p201ln.pdf'>201</a> |
-        <a href='resources/2012p202ln.pdf'>202</a> |
-        <a href='resources/2012p203ln.pdf'>203</a>
-    </li>
-    <li>2011 Lecture Notes<br>
-        <a href='resources/2011p200ln.pdf'>201&ndash;202&ndash;203</a>
-    </li>
-</ul>
+    <div id="col2">
 
-<p>Still looking for help? Check out <a href="http://physicsforums.com">PhysicsForums.com</a>!</p>
+        <h2>Class docs</h2>
+        <?php echo $docs; ?>
 
-<h2>My contact info</h2>
-<dl>
-    <dt>Instructor</dt>
-    <dd>David J. Ulrich</dd>
-    <dt>E-mail</dt>
-    <dd><a href='mailto:david.ulrich15@pcc.edu'>david.ulrich15@pcc.edu</a></dd>
-    <dt>Time</dt>
-    <dd>Monday, Wednesday <nobr>6:00-8:50 pm</nobr></dd>
-    <dt>Location</dt>
-    <dd>PCC, Rock Creek Campus</dd>
-    <dd>Building 7</dd>
-    <dt>Room</dt>
-    <dd>223 on Monday</dd>
-    <dd>225 on Wednesday</dd>
-</dl>
+        <h2>Extra docs</h2>
+        <?php echo $supp; ?>
 
-<h2>Related courses</h2>
-<ul>
-    <li><a href="?qtr=201">201</a></li>
-    <li><a href="?qtr=202">202</a></li>
-    <li><a href="?qtr=203">203</a></li>
-</ul>
+        <h2>General docs</h2>
+        <ul>
+            <li><a href='resources/si-units.pdf'>SI units</a></li>
+            <li><a href='resources/constants.pdf'>Constants</a></li>
+            <li><a href='resources/notations.pdf'>Notations</a></li>
+        </ul>
 
-<p>Site hosting by Portland Community College, <a href='http://www.pcc.edu'>http://www.pcc.edu</a></p>
+        <h2>My archives...</h2>
+        <ul>
+            <li>2012 Lecture Notes<br>
+                <a href='resources/2012p201ln.pdf'>201</a> |
+                <a href='resources/2012p202ln.pdf'>202</a> |
+                <a href='resources/2012p203ln.pdf'>203</a>
+            </li>
+            <li>2011 Lecture Notes<br>
+                <a href='resources/2011p200ln.pdf'>201&ndash;202&ndash;203</a>
+            </li>
+        </ul>
+
+        <p>Still looking for help? Check out <a href="http://physicsforums.com">PhysicsForums.com</a>!</p>
+        
+    </div>
+
+    <div id="col3" class="lastcol">
+
+        <h2>My contact info</h2>
+        <dl>
+            <dt>Instructor</dt>
+            <dd>David J. Ulrich</dd>
+            <dt>E-mail</dt>
+            <dd><a href='mailto:david.ulrich15@pcc.edu'>david.ulrich15@pcc.edu</a></dd>
+            <dt>Time</dt>
+            <dd>Monday, Wednesday <nobr>6:00-8:50 pm</nobr></dd>
+            <dt>Location</dt>
+            <dd>PCC, Rock Creek Campus</dd>
+            <dd>Building 7</dd>
+            <dt>Room</dt>
+            <dd>223 on Monday</dd>
+            <dd>225 on Wednesday</dd>
+        </dl>
+
+        <h2>Related courses</h2>
+        <ul>
+            <li><a href="?qtr=201">201</a></li>
+            <li><a href="?qtr=202">202</a></li>
+            <li><a href="?qtr=203">203</a></li>
+        </ul>
+
+    </div>
+
+    <footer>
+
+        <p>Site hosting by Portland Community College, <a href='http://www.pcc.edu'>http://www.pcc.edu</a></p>
+        
+    </footer>
 
 </body>
 </html>
