a1fdadc6fd80ea6eccf6bf032e6009ede160fbe5
Dave <none@example.com>
2013-01-07 21:59:44 -0800

    Fixed bug in login/grades

--- grades.php
+++ grades.php
@@ -4,8 +4,8 @@ session_start();
 include_once "utils.php";
 
 // If the assignments file is not set up, we are not ready ...
-if ( ! file_exists($_SESSION['lib'] . "/meta/assignments.csv" ) )
-header ( "location:no_grades.php" );
+if ( ! file_exists( "$lib/$meta/assignments.csv" ) )
+header ( "Location: no_grades.php" );
 
 // Otherwise, redirect based on access level ...
 if      ( $user['access'] == 2 ) header ( "Location: list_students.php" );
